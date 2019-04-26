08c348fedf8b4cfa01ba11c1ad26afef931fefdf
Jaspreet Rai <JRai@spartaglobal.com>
2019-04-26 10:44:16 +0100

    Stack Queue tests all work

--- lab_102_homework/Program.cs
+++ lab_102_homework/Program.cs
@@ -10,20 +10,26 @@ namespace lab_102_homework
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Collections.UseCollections(1, 2, 3, 4, 5));
+            Console.WriteLine(Collections.UseCollections(1, 2, 3, 4, 1));
+            Console.WriteLine(Collections.UseCollections(1, 1, 1, 1, 1));
         }
     }
     public class Collections
     {
-        static List<int> list01 = new List<int>();
-        static Stack<int> stack01 = new Stack<int>();
-        static Queue<int> queue01 = new Queue<int>();
+      
 
         public static int UseCollections(int a,int b, int c, int d, int e)
-        {
-            //Create a list with 5 items 
+        {   //Create a list with 5 items 
             //then iterate over the list :  multiple each item by 2: Add to a Stack!
             //then iterate over the stack: add 10 over each number: add to queue!
             //Iterate over queue: return total
+
+            List<int> list01 = new List<int>();
+            Stack<int> stack01 = new Stack<int>();
+            Queue<int> queue01 = new Queue<int>();
+
+            
             list01.Add(a);
             list01.Add(b);
             list01.Add(c);
@@ -38,8 +44,33 @@ namespace lab_102_homework
             {
                 queue01.Enqueue(S+10);
             }
+            
             int sum = queue01.Sum();
             return sum;
+
+            /*
+            // LUITZEN's CODE
+            for (int i = 0; i < list01.Count; i++)
+            {
+                stack01.Push(2 * list01[i]);
+            }
+            while(stack01.Count != 0)
+            {
+                queue01.Enqueue(10 + stack01.Pop());
+            }
+            var sum = 0;
+            while (queue01.Dequeue !=0)
+            {
+            sum += queue01.Dequeue();
+            }
+            return sum;
+            */
+
+
+
+
+
+
         }
     }
 
---
+++ lav_47_break_continue/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lav_47_break_continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i==5 || i==6) 
+                {
+                    continue; //skips if ints and continues the loop
+                }
+                Console.WriteLine(i);
+            }
+            int counter = 0;
+            while (counter < 10)
+            {
+                if (counter == 8)
+                {
+                    break; //stops the loop
+                }
+                Console.WriteLine( counter );
+                counter++;
+                
+            }
+          
+        }
+    }
+}
