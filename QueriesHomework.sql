b7cfcb6012c4b87d433bb6a5e0f1c82d6948bf4b
jaspreetkaur28 <jaspreet_kaur_rai@hotmail.co.uk>
2019-05-08 16:53:05 +0100

    SQL Homework

---
+++ QueriesHomework.sql
@@ -0,0 +1,18 @@
+﻿
+use Northwind 
+select sum(Quantity) As 'Total Stock' from [Order Details ]
+select avg(UnitPrice) As 'Total average' from [Order Details ]
+select Min(UnitPrice) As 'Min', Max(UnitPrice) As 'Max' from [Order Details ]
+select count(*) As 'Total Orders' from orders
+select count(CustomerID) As 'Number of customers', Country FROM Customers GROUP BY Country Having Count(CustomerID) >6 Order by [Number of customers] Desc; 
+select  [Order Details].OrderID,  Products.ProductName,  [Order Details].UnitPrice,  [Order Details].Quantity from [Order Details] 
+Inner join Products on Products.ProductID =  [Order Details].ProductID
+/* create some queries using these  keywords on the Northwind database 
+1. sum 
+2. avg 
+3. Min/max 
+4. count 
+5. group by ??? 
+6. having ??? 
+7. inner join
+*/
