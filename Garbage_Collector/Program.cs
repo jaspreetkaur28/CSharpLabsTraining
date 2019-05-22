c5a04da66f60acf807e7b3c78a1a7cfeb1985b39
jaspreetkaur28 <jaspreet_kaur_rai@hotmail.co.uk>
2019-05-22 10:12:03 +0100

    update

---
+++ Garbage_Collector/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garbage_Collector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GC.Collect(); 
+        }
+    }
+}
